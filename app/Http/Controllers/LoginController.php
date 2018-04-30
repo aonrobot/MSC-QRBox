7017e57f513df90f8b322d828d98701ce4b0f68f
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2018-04-30 16:58:11 +0700

    must have user in member table only

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,27 +8,36 @@ use Illuminate\Support\Facades\Auth;
 use Crypt;
 use Session;
 
+use App\Member;
+
 class LoginController extends Controller
 {
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('guest')->except('logout');
     }
 
-    public function do(Request $request){
+    public function do(Request $request) {
         
         $username = $request->input('username');    
         $password = $request->input('password');
-        if (Auth::attempt(['samaccountname' => $username, 'password' => $password])) {
-            $user = Auth::user();
-            Session::put('basic-auth', Crypt::encryptString($user->getAccountName()));
-            return redirect('/')->with('message', 'Logged in!');
+
+        $count_exist = Member::where('loginUser', $username)->count();
+
+        if($count_exist){
+            if (Auth::attempt(['samaccountname' => $username, 'password' => $password])) {
+                $user = Auth::user();
+                Session::put('basic-auth', Crypt::encryptString($user->getAccountName()));
+                return redirect('/')->with('message', 'Logged in!');
+            }else{
+                return redirect('login')->with('message', 'Password หรือ Username ผิด');
+            }
         }
         
-        return redirect('login')->with('message', 'Password หรือ Username ผิด');
+        return redirect('login')->with('message', 'กรุณาสมัครสมาชิกก่อนเข้าใช้งาน');
+        
     }
 
-    public function logout(){
+    public function logout() {
         Session::forget('basic-auth');
         Session::flush();
         Auth::logout();
